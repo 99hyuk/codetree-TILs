f574320383c9d52cee50fc18a8ed7d66d6b737bc
조용혁 <95476328+99hyuk@users.noreply.github.com>
2025-11-19 16:59:07 +0900

    Title: 문자열 정렬, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/문자열 정렬/string-sort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
+        char[] str = s.toCharArray();
+        Arrays.sort(str);
+        System.out.print(str);
+    }
+}
