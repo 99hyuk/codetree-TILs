84ae8afef4de1987397f7a7a6d816e60473382c3
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-22 16:08:32 +0900

    Title: 서로 다른 BST 개수 세기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 260422/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -5,13 +5,14 @@ public class Main {
         int n = sc.nextInt();
         // Please write your code here.
         int[] dp = new int[20];
+        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 5;
 
         for (int i=4; i<=19; i++) {
-            for (int j=1; j<i; j++) {
-                dp[i] += dp[j-1] + dp[i-j];
+            for (int j=1; j<=i; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
             }
         }
 
