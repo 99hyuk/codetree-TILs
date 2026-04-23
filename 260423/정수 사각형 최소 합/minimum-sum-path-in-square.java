92a99c73369821df5505af4805a08046d07bec54
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-23 11:50:10 +0900

    Title: 정수 사각형 최소 합, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260423/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    static class Point implements Comparable<Point> {
+        int r,c,t;
+        Point(int r, int c ,int t) {
+            this.r = r;
+            this.c = c;
+            this.t = t;
+        }
+        public int compareTo(Point o) {
+            return o.t - this.t;
+        }
+    }
+    static int dr[] = {1,0};
+    static int dc[] = {0,-1};
+    static int N, map[][];
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        map = new int[N][N];
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+
+        System.out.print(bfs());
+    }
+
+    static int bfs() {
+        int dist[][] = new int[N][N];
+        for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        dist[0][N-1] = map[0][N-1];
+        PriorityQueue <Point> Q = new PriorityQueue<>();
+        Q.offer(new Point(0,N-1,map[0][N-1]));
+
+        while(!Q.isEmpty()) {
+            Point p = Q.poll();
+            if(dist[p.r][p.c] < p.t) continue;
+
+            for(int i=0; i<2; i++) {
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+
+                if(nr <0 || nr >= N || nc <0 || nc >= N) continue;
+                if(dist[nr][nc] > dist[p.r][p.c] + map[nr][nc]) {
+                    dist[nr][nc] = dist[p.r][p.c] + map[nr][nc];
+                    Q.offer(new Point(nr,nc,dist[nr][nc]));
+                }
+            }
+        }
+        return dist[N-1][0];
+    }
+}
