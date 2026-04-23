d17586e732e4a1c19c281faefd20ab5d05878280
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-23 12:38:58 +0900

    Title: 정수 사각형 최소 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/정수 사각형 최소 합/minimum-sum-path-in-square.java
+++ 260423/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -1,57 +1,31 @@
 import java.util.*;
-import java.io.*;
+
 public class Main {
-    static class Point implements Comparable<Point> {
-        int r,c,t;
-        Point(int r, int c ,int t) {
-            this.r = r;
-            this.c = c;
-            this.t = t;
-        }
-        public int compareTo(Point o) {
-            return this.t-o.t;
-        }
-    }
-    static int dr[] = {1,0};
-    static int dc[] = {0,-1};
-    static int N, map[][];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        for (int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] map = new int[N+2][N+2];
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                map[i][j] = sc.nextInt();
             }
         }
 
-
-        System.out.print(bfs());
+    int[][] dp = new int[N+2][N+2];
+    for (int i=1; i<=N; i++) {
+        dp[1][N-i+1] = dp[1][N-i+2] + map[1][N-i+1];
+        dp[i][N] = dp[i-1][N] + map[i][N];
     }
 
-    static int bfs() {
-        int dist[][] = new int[N][N];
-        for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        dist[0][N-1] = map[0][N-1];
-        PriorityQueue <Point> Q = new PriorityQueue<>();
-        Q.offer(new Point(0,N-1,map[0][N-1]));
-
-        while(!Q.isEmpty()) {
-            Point p = Q.poll();
-            if(dist[p.r][p.c] < p.t) continue;
-
-            for(int i=0; i<2; i++) {
-                int nr = p.r + dr[i];
-                int nc = p.c + dc[i];
-
-                if(nr <0 || nr >= N || nc <0 || nc >= N) continue;
-                if(dist[nr][nc] > dist[p.r][p.c] + map[nr][nc]) {
-                    dist[nr][nc] = dist[p.r][p.c] + map[nr][nc];
-                    Q.offer(new Point(nr,nc,dist[nr][nc]));
-                }
-            }
+    for (int i=2; i<=N; i++) {
+        for (int j=N-1; j>=1; j--) {
+            dp[i][j] = Math.min(dp[i-1][j], dp[i][j+1]) + map[i][j]; 
+            // System.out.print(dp[i][j]  + " ");
         }
-        return dist[N-1][0];
+        // System.out.println();
     }
+
+    System.out.print(dp[N][1]);
+}
 }
