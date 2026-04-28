95fe23d23d470d15be1368ed3ac3856e05f0d26b
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-28 14:58:49 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260428/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] map = new int[N+1][N+1];
+        int[][] dp = new int[N+1][N+1];
+        
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        dp[1][1] = map[1][1];
+        for (int i=2; i<=N; i++) {
+            dp[1][i] = Math.min(map[1][i], dp[1][i-1]);
+            dp[i][1] = Math.min(map[i][1], dp[i-1][1]);
+        }
+
+        for (int i=2; i<=N; i++) {
+            for (int j=2; j<=N; j++) {
+                dp[i][j] = Math.min(map[i][j], Math.min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+
+        int maxOfMin = Integer.MIN_VALUE;
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                maxOfMin = Math.max(maxOfMin, dp[i][j]);
+            }
+        } 
+        System.out.print(max);
+    }
+}
