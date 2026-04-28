e467275dfaeff8666f39735ef2bd537c48b8547c
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-28 17:14:04 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/정수 사각형 최댓값의 최소/minimax-path-in-square.java
+++ 260428/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -15,7 +15,16 @@ public class Main {
         dp[0][0] = matrix[0][0];
 
         for (int i=1; i<n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], mat)
+            dp[i][0] = Math.max(dp[i-1][0], matrix[i][0]);
+            dp[0][i] = Math.max(dp[0][i-1], matrix[0][i]);
         }
+
+        for (int i=1; i<n; i++) {
+            for (int j=1; j<n; j++) {
+                dp[i][j] = Math.max(matrix[i][j], Math.min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+
+        System.out.print(dp[n-1][n-1]);
     }
 }
