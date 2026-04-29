5d39866803d2c2ab9fc83458b1b9e83bf869a9c2
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-29 14:58:14 +0900

    Title: 최대 증가 부분 수열, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260429/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 260429/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -15,7 +15,7 @@ public class Main {
         
         int[] dp = new int[N+1];
         dp[1] = 1;
-        int max = 0;
+        int max = 1;
 
         for (int i=1; i<=N; i++) {
             for (int j=1; j<i; j++) {
