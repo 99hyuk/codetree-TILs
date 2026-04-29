ae395073b9afab3f5bdd94be383bd14b8c235910
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-29 18:16:54 +0900

    Title: 2차원 최대 증가 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 260429/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -17,13 +17,14 @@ public class Main {
         
         int[][] dp = new int[N+1][M+1];
         int max = 0;
+        dp[1][1] = 1;
 
         for (int r1=1; r1<=N; r1++) {
             for (int c1=1; c1<=M; c1++) {
-                dp[r1][c1] = 1;
+                
                 for (int r2=1; r2<r1; r2++) {
                     for (int c2=1; c2<c1; c2++) {
-                        if(r1==r2 && c1==c2) continue;
+                        if(dp[r2][c2] < 1) continue;
 
                         if(grid[r1][c1] > grid[r2][c2]) {
                             dp[r1][c1] = Math.max(dp[r1][c1], dp[r2][c2] + 1);
@@ -33,6 +34,7 @@ public class Main {
                 }
             }
         }
+        
 
         System.out.print(max);
     }
