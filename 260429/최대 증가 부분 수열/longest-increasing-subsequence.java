0a7e6b97b82a40537677a43e00cf1750504c20de
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-04-29 15:04:02 +0900

    Title: 최대 증가 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 260429/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -14,15 +14,15 @@ public class Main {
         }
         
         int[] dp = new int[N+1];
-        dp[1] = 1;
+        Arrays.fill(dp, 1);
         int max = 1;
 
         for (int i=1; i<=N; i++) {
             for (int j=1; j<i; j++) {
                 if (arr[j] < arr[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
-                    max = Math.max(max, dp[i]);
                 }
+                    max = Math.max(max, dp[i]);
             }
         }
         
